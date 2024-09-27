54344e6da665fab23d90f55fc567456baff57593
lebresilien <petitotapondjou@gmail.com>
2024-09-27 14:06:35 +0100

    animate spin on hover

---
+++ src/app/fun/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center space-y-5">
+            <div className="w-96 space-y-3 flex flex-col cursor-pointer border border-blue-700 hover:animate-spin p-5">
+                <span className="font-bold text-lg text-center uppercase">john doe</span>
+                <p className="font-bold text-sm text-gray-500 mb-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </main>
+    )
+}
